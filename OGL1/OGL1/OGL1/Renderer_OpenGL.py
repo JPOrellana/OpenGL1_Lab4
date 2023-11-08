a23da629a19a98fc26b935c7515b01eff868abfb
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-11-07 18:47:18 -0600

    Laboratorio terminado

--- OGL1/OGL1/OGL1/Renderer_OpenGL.py
+++ OGL1/OGL1/OGL1/Renderer_OpenGL.py
@@ -1,11 +1,11 @@
-from OpenGL.GL.shaders import fragment_shader, vertex_shader
-from OpenGL.GL import glUniform1f, glGetUniformLocation
+import glm
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
 import pygame
-from pygame.locals import * 
+from pygame.locals import *
 from gl import Renderer
 from model import Model
 from shaders import *
-import glm
 
 width = 1000
 height = 1000
@@ -17,56 +17,64 @@ clock = pygame.time.Clock()
 
 rend = Renderer(screen)
 
-rend.setShader(vertexShader = rainbow_vertex_shader, fragmentShader = rainbow_fragment_shader)
+# Inicialmente, establecemos el shader "rainbow" al inicio
+rend.setShader(vertexShader=rainbow_vertex_shader, fragmentShader=rainbow_fragment_shader)
 
-
-model = Model("modelos/carro.obj")
-model.loadTexture("texturas/carro.bmp")
+model = Model("modelos/cala.obj")
+model.loadTexture("texturas/ola.bmp")
 model.position.z = -5.5
-model.scale = glm.vec3(2,2,2)
+model.scale = glm.vec3(2, 2, 2)
 model.rotation.x = 45
 
 rend.scene.append(model)
 
 isRunning = True
 
+current_shader = 1  # 1 for rainbow, 2 for grid, 3 for waves
+
 while isRunning:
+    deltaTime = clock.tick(60) / 1000
+    keys = pygame.key.get_pressed()
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                isRunning = False
 
-	deltaTime = clock.tick(60) / 1000
-	keys = pygame.key.get_pressed()
+            # Cambia el shader al presionar teclas 1, 2 o 3
+            if event.key == pygame.K_1:
+                if current_shader != 1:
+                    rend.setShader(vertexShader=rainbow_vertex_shader, fragmentShader=rainbow_fragment_shader)
+                    current_shader = 1
+            elif event.key == pygame.K_2:
+                if current_shader != 2:
+                    rend.setShader(vertexShader=grid_vertex_shader, fragmentShader=grid_fragment_shader)
+                    current_shader = 2
+            elif event.key == pygame.K_3:
+                if current_shader != 3:
+                    rend.setShader(vertexShader=waves_vertex_shader, fragmentShader=waves_fragment_shader)
+                    current_shader = 3
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			isRunning = False
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				isRunning = False
+    if keys[K_d]:
+        rend.camPosition.x -= 5 * deltaTime
+    if keys[K_a]:
+        rend.camPosition.x += 5 * deltaTime
+    if keys[K_w]:
+        rend.camPosition.y -= 5 * deltaTime
+    if keys[K_s]:
+        rend.camPosition.y += 5 * deltaTime
+    if keys[K_q]:
+        rend.camPosition.z -= 5 * deltaTime
+    if keys[K_e]:
+        rend.camPosition.z += 5 * deltaTime
 
-	if keys[K_d]:
-		rend.camPosition.x -= 5 * deltaTime
-	
-	if keys[K_a]:
-		rend.camPosition.x += 5 * deltaTime
+    if rend.activeShader == waves_vertex_shader:
+        glUniform1f(glGetUniformLocation(rend.activeShader, "time"), rend.elapsedTime)
 
-	if keys[K_w]:
-		rend.camPosition.y -= 5 * deltaTime
-	
-	if keys[K_s]:
-		rend.camPosition.y += 5 * deltaTime
-
-	if keys[K_q]:
-		rend.camPosition.z -= 5 * deltaTime
-	
-	if keys[K_e]:
-		rend.camPosition.z += 5 * deltaTime
-
-	if rend.activeShader == rainbow_vertex_shader:
-		glUniform1f(glGetUniformLocation(rend.activeShader, "time"), rend.elapsedTime)
-
-	rend.elapsedTime += deltaTime
-	rend.render()
-	pygame.display.flip()
-
+    rend.elapsedTime += deltaTime
+    rend.render()
+    pygame.display.flip()
 
 pygame.quit()
--- OGL1/OGL1/OGL1/shaders.py
+++ OGL1/OGL1/OGL1/shaders.py
@@ -112,18 +112,74 @@ out vec4 fragmentColor;
 
 void main()
 {
+    int gridSize = 30;  
+    vec2 gridUV = floor(uvs * float(gridSize));
+    
+    float lineThickness = 0.1;  
 
-    int gridSize = 1;
-    vec2 gridUV = floor(uvs * float(gridSize)) / float(gridSize);
+    float horizontalLine = step(mod(uvs.y * gridSize, 1.0), lineThickness);
+    float verticalLine = step(mod(uvs.x * gridSize, 1.0), lineThickness);
 
     if (mod(int(gridUV.x) + int(gridUV.y), 2) == 0) {
-        fragmentColor = vec4(0.0, 1.0, 0.0, 1.0);  // Color verde
+        fragmentColor = vec4(0.255, 0.157, 0.031, 1.0);  
     } else {
-        fragmentColor = vec4(1.0, 0.0, 0.0, 1.0);  // Color rojo
+        fragmentColor = vec4(0.097, 0.179, 0.033, 1.0);  
+    }
+
+    if (horizontalLine > 0.0 || verticalLine > 0.0) {
+        fragmentColor = vec4(0.0, 0.0, 0.0, 1.0);  
     }
 }
+
 '''
 # ------------------------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------- Shader 3: Ondas/Olas -----------------------------------------
+waves_vertex_shader = '''
+#version 450 core
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 texCoords;
+
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+uniform float time;
+
+out vec2 uvs;
+
+void main()
+{
+    vec4 newPos = vec4(position.x, position.y + sin(time) / 4.0, position.z, 1.0);
+
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(newPos);
+    uvs = texCoords;
+}
+'''
+
+waves_fragment_shader = '''
+#version 450 core
+
+uniform float time;
+uniform sampler2D tex;  // Agrega una textura
+
+in vec2 uvs;
+out vec4 fragmentColor;
+
+void main()
+{
+    float frequency = 2.0;
+    float speed = 0.1;
+    vec2 offset = vec2(sin(time * speed), cos(time * speed));
+    vec2 disturbedUV = uvs + offset * sin(uvs.x * frequency);
+    vec3 waterColor = vec3(0.006, 0.112, 0.231);
+    fragmentColor = vec4(mix(waterColor, texture(tex, disturbedUV).xyz, 0.7), 1.0); 
+}
+
+
+'''
+# -----------------------------------------------------------------------------------------------------------
 
 
 
